708ba7839c566a97b59dfcf76b23ea3e13177640
Grey <greysdevmail@gmail.com>
2023-08-24 20:33:39 +0200

    traitor: Update in-code license header

--- src/traitor.zig
+++ src/traitor.zig
@@ -2,20 +2,21 @@
 // domain exists. Alternatively, it is available under the Zero-Clause BSD
 // license.
 // ------------------------------------------------------------------------------
-// Zero-Clause BSD
-// Copyright (c) 2023 greytdepression <greysdevmail@gmail.com>
-// =============
+// BSD Zero Clause License (SPDX: 0BSD)
 //
-// Permission to use, copy, modify, and/or distribute this software for
-// any purpose with or without fee is hereby granted.
+// ----------------------------------------------------------------------
 //
-// THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL
-// WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
-// OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
-// FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
-// DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
-// AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
-// OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// Permission to use, copy, modify, and/or distribute this software
+// for any purpose with or without fee is hereby granted.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+// WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+// THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+// CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+// NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+// WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 const std = @import("std");
 const trait = std.meta.trait;
